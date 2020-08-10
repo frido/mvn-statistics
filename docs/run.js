d4d85ef7057dc6a67f978772637bc383c622fe1f
frido <peter.fridrich@outlook.sk>
2020-08-11 00:01:07 +0200

    new src-docs folder

--- docs/run.js
+++
@@ -1,9 +0,0 @@
-fs = require('fs');
-var md = require('markdown-it')();
-
-fs.readFile('../README.md', "utf8", function(err, data) {
-    var result = md.render(data);
-    fs.writeFile('index.html', result, function (err) {
-        if (err) return console.log(err);
-    });
-});
